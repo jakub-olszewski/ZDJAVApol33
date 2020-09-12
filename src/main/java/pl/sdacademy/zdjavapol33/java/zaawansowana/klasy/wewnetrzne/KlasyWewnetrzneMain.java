341cec42d9a33702baa5eabd56a00635ae555a83
Jakub Olszewski <j.b.olszewski@gmail.com>
2020-09-12 12:30:33 +0200

    Teoria: Klasa wewnetrzna

---
+++ src/main/java/pl/sdacademy/zdjavapol33/java/zaawansowana/klasy/wewnetrzne/KlasyWewnetrzneMain.java
@@ -0,0 +1,17 @@
+package pl.sdacademy.zdjavapol33.java.zaawansowana.klasy.wewnetrzne;
+
+/**
+ * @author : Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @project : ZDJAVApol33
+ * @since : 12.09.2020
+ **/
+public class KlasyWewnetrzneMain {
+
+    public static void main(String[] args) {
+
+        Segregator dokumenty = new Segregator("Dokumenty");
+        dokumenty.utworzZakladke("Rachunki za prad");
+
+
+    }
+}
---
+++ src/main/java/pl/sdacademy/zdjavapol33/java/zaawansowana/klasy/wewnetrzne/Segregator.java
@@ -0,0 +1,25 @@
+package pl.sdacademy.zdjavapol33.java.zaawansowana.klasy.wewnetrzne;
+
+import java.util.ArrayList;
+
+/**
+ * @author : Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @project : ZDJAVApol33
+ * @since : 12.09.2020
+ **/
+public class Segregator {
+
+    private String tytul;
+
+    public Segregator(String tytul) {
+        this.tytul = tytul;
+    }
+
+    public void utworzZakladke(String tytul) {
+
+    }
+
+    public class Zakladka extends ArrayList {
+
+    }
+}
