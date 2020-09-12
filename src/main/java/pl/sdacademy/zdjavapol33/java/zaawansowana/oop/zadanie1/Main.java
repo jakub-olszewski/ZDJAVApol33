19b021b5950297ee54ee6fa0d3f0808bdaccb409
Jakub Olszewski <j.b.olszewski@gmail.com>
2020-09-12 09:57:38 +0200

    Zadanie: OOP Figury

---
+++ src/main/java/pl/sdacademy/zdjavapol33/java/zaawansowana/oop/zadanie1/Main.java
@@ -0,0 +1,13 @@
+package pl.sdacademy.zdjavapol33.java.zaawansowana.oop.zadanie1;
+
+/**
+ * @author : Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @project : ZDJAVApol33
+ * @since : 12.09.2020
+ **/
+public class Main {
+
+    public static void main(String[] args) {
+        // Zadanie z figurami
+    }
+}
