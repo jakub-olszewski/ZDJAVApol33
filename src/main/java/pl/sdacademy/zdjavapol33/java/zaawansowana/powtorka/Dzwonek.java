673c07b6a41791831a1dfa4f45884eb53de95c11
Jakub Olszewski <j.b.olszewski@gmail.com>
2020-09-13 11:38:11 +0200

    Powtorka: Funcjonalność boolean

---
+++ src/main/java/pl/sdacademy/zdjavapol33/java/zaawansowana/powtorka/Dzwonek.java
@@ -0,0 +1,32 @@
+package pl.sdacademy.zdjavapol33.java.zaawansowana.powtorka;
+
+/**
+ * @author : Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @project : ZDJAVApol33
+ * @since : 13.09.2020
+ **/
+public class Dzwonek {
+
+    private boolean przyciskWcisniety;
+
+    public Dzwonek() {
+        this.dzwiek = "aaa kotki dwa...";
+    }
+
+    private String dzwiek;
+
+    public void posluchaj() {
+        System.out.println("słucham...");
+        if(przyciskWcisniety){
+            // dzwoni
+            System.out.println(dzwiek);
+        }else{
+            // nie dzwoni
+        }
+    }
+
+    public void wcisnij() {
+        System.out.println("wcisniecie przycisku");
+        przyciskWcisniety = true;
+    }
+}
---
+++ src/main/java/pl/sdacademy/zdjavapol33/java/zaawansowana/powtorka/FuncjonalnoscMain.java
@@ -0,0 +1,18 @@
+package pl.sdacademy.zdjavapol33.java.zaawansowana.powtorka;
+
+/**
+ * @author : Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @project : ZDJAVApol33
+ * @since : 13.09.2020
+ **/
+public class FuncjonalnoscMain {
+
+    public static void main(String[] args) {
+
+        Dzwonek dzwonekDoDrzwi = new Dzwonek();
+        dzwonekDoDrzwi.posluchaj();
+        dzwonekDoDrzwi.wcisnij();
+        dzwonekDoDrzwi.posluchaj();
+
+    }
+}
