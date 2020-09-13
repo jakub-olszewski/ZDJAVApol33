86fc05c75f7865f356b06478e9dbfb41d0e3259a
Jakub Olszewski <j.b.olszewski@gmail.com>
2020-09-13 13:44:25 +0200

    Teoria: Kolekcje

---
+++ src/main/java/pl/sdacademy/zdjavapol33/java/zaawansowana/kolekcje/KolekcjeZadanie1Main.java
@@ -0,0 +1,59 @@
+package pl.sdacademy.zdjavapol33.java.zaawansowana.kolekcje;
+
+import pl.sdacademy.zdjavapol33.java.zaawansowana.kolekcje.lista.DworzecGlownychMcDonaldImpl;
+import pl.sdacademy.zdjavapol33.java.zaawansowana.kolekcje.lista.McDonald;
+import pl.sdacademy.zdjavapol33.java.zaawansowana.kolekcje.lista.PlazaMcDonaldImpl;
+import pl.sdacademy.zdjavapol33.java.zaawansowana.powtorka.Przedmiot;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author : Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @project : ZDJAVApol33
+ * @since : 13.09.2020
+ **/
+public class KolekcjeZadanie1Main {
+
+    public static void main(String[] args) {
+        /**
+         * Zadanie.
+         *
+         * Podaj przyklady użyć poniższych kolekcji
+         * Zaaragumentuj wybrane użycie
+
+         * Czym się różni lista od zbioru ?
+         * -- lista ma kolejnosc a zbiór nie, interacja po elementach
+         *
+         * Przykłady:
+         * - struktura Ksiązek -
+         *      - Set zbiór książek
+         *      - Map jeśli chce wyszukać np po ISBN
+         * - struktura Stacji paliw
+         *      - Set zbiór
+         * - struktura słownika tłumaczeń
+         *      - Map szukamy według klucza słowo
+         * - struktura bazy ZUS ubezpieczonych
+         *      - Map kluczem jest pesel lub nip
+         * - struktura bazy PLUS telefonii
+         *      - Map kluczem jest numer telefonu
+         * - struktura ogłoszeń na allegro
+         *      - List jeśli ważna jest kolejność dodawanych ogłoszeń
+         *      - Set jeśli chcemy przechowywać ogłoszenia
+         *      - Map jeśli kluczem jest numer ogłoszenia
+         * - struktura kody pocztowe
+         *      - Map kluczem jest kod
+         * - struktura restauracji menu
+         *      - List ważna jest kolejność pozycji w menu
+         */
+
+        // lista
+        /**
+         * indeks -> uczeń
+         * w przypadku gdy lista jest pokolei
+         * - kolejność
+         */
+
+    }
+}
