1a7328dbbbd081ec8d3e43b526109fe968f3c2f3
Jakub Olszewski <j.b.olszewski@gmail.com>
2020-09-13 10:39:51 +0200

    Powtorka: Wywolanie konstruktora w konstrukorze

---
+++ src/main/java/pl/sdacademy/zdjavapol33/java/zaawansowana/powtorka/KonstruktorMain.java
@@ -0,0 +1,16 @@
+package pl.sdacademy.zdjavapol33.java.zaawansowana.powtorka;
+
+/**
+ * @author : Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @project : ZDJAVApol33
+ * @since : 13.09.2020
+ **/
+public class KonstruktorMain {
+
+    public static void main(String[] args) {
+
+        Most mostWarszawski = new Most();// uzycie konsturktora domyslnego bez argumentowego
+
+        new Most("Wiedeński");
+    }
+}
