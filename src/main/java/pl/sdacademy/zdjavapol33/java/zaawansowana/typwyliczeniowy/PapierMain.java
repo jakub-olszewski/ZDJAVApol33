01c59d5f00b6b1948f0deaef181de6a497ab7260
Jakub Olszewski <j.b.olszewski@gmail.com>
2020-09-19 10:34:01 +0200

    Zadanie: Typ wyliczeniowy

---
+++ src/main/java/pl/sdacademy/zdjavapol33/java/zaawansowana/typwyliczeniowy/PapierMain.java
@@ -0,0 +1,24 @@
+package pl.sdacademy.zdjavapol33.java.zaawansowana.typwyliczeniowy;
+
+/**
+ * @author : Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @project : ZDJAVApol33
+ * @since : 19.09.2020
+ **/
+public class PapierMain {
+
+    public static void main(String[] args) {
+        /**
+         * Zadanie z wykorzystaniem typu wyliczeniowego
+         */
+
+        // A1, A2 ,A3,A4
+        // Metody:
+        // pobierzDlugosc()
+        // pobierzSzerokosc()
+        //
+        // Konstruktor - na długość i szerokość
+
+        // w klasie main utworzyc obiekt A3 i wypisać rozmiar na konsolę
+    }
+}
---
+++ src/main/java/pl/sdacademy/zdjavapol33/java/zaawansowana/typwyliczeniowy/RozmiarPapier.java
@@ -0,0 +1,9 @@
+package pl.sdacademy.zdjavapol33.java.zaawansowana.typwyliczeniowy;
+
+/**
+ * @author : Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @project : ZDJAVApol33
+ * @since : 19.09.2020, So
+ **/
+public enum RozmiarPapier {
+}
