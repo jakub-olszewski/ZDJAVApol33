7c3804f4ae02991f5e059098176b8775182543d0
Jakub Olszewski <j.b.olszewski@gmail.com>
2020-09-19 12:51:08 +0200

    Zadanie: Typy generyczne

---
+++ src/main/java/pl/sdacademy/zdjavapol33/java/zaawansowana/typygeneryczne/zadanie1/Zadanie1Main.java
@@ -0,0 +1,29 @@
+package pl.sdacademy.zdjavapol33.java.zaawansowana.typygeneryczne.zadanie1;
+
+/**
+ * @author : Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @project : ZDJAVApol33
+ * @since : 19.09.2020
+ **/
+public class Zadanie1Main {
+
+    /**
+     * Typ wyliczeniowy, Dziedziczenie, Typy generyczne
+     *
+     * Paczkomat
+     * - Paczkomat - klasa główna { lokalizacja, nazwa}
+     * - WarszawaPaczkomat - dziedziczy po klasie paczkomat
+     * - w argumencie podjemy obiekt Paczka<Z>
+     *     { dlugosc, szerokosc, wysokosc, waga}
+     * - typ wyliczeniowy paczka rozmar A, B ,C
+     * gabaryt A – maksymalne wymiary 8 X 38 X 64 cm, maksymalna waga: 25 kg;
+     * gabaryt B – maksymalne wymiary 19 X 38 X 64 cm, maksymalna waga: 25 kg;
+     * gabaryt C – maksymalne wymiary 41 X 38 X 64 cm, maksymalna waga: 25 kg.
+     * - zrzucanie wyjatku w przypadku paczki ciezszej niz 25 kg
+     * - zwracanie ceny paczki w zależności od gabarytów
+     * gabaryt A – 12,99
+     * gabaryt B – 13,99
+     * gabaryt C – 15,99
+     *
+     */
+}
