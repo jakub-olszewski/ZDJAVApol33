8f4d65bf40c5b179c81d250a48c3d0f5d4428912
Jakub Olszewski <j.b.olszewski@gmail.com>
2020-09-20 13:56:36 +0200

    Teoria: Optional

---
+++ src/main/java/pl/sdacademy/zdjavapol33/java/zaawansowana/optional/OptionalMain.java
@@ -0,0 +1,24 @@
+package pl.sdacademy.zdjavapol33.java.zaawansowana.optional;
+
+import pl.sdacademy.zdjavapol33.java.zaawansowana.adnotacje.JavaTopic;
+import pl.sdacademy.zdjavapol33.java.zaawansowana.adnotacje.MainClass;
+
+/**
+ * @author : Jakub Olszewski [http://github.com/jakub-olszewski]
+ * @project : ZDJAVApol33
+ * @since : 20.09.2020
+ **/
+@JavaTopic(topic = "Programowanie funkcyjne", descripion = "Optional")
+public class OptionalMain extends MainClass {
+
+    public OptionalMain(){
+        super();
+    }
+
+    public static void main(String[] args) {
+        new OptionalMain();
+
+
+
+    }
+}
