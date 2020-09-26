1431528c87bf3ad1fdf0ea6e52777dbce1cebd4a
Jakub Olszewski <j.b.olszewski@gmail.com>
2020-09-26 09:17:48 +0200

    Rozwiazania: Stream

--- src/main/java/pl/sdacademy/zdjavapol33/java/zaawansowana/stream/StreamMain.java
+++ src/main/java/pl/sdacademy/zdjavapol33/java/zaawansowana/stream/StreamMain.java
@@ -1,6 +1,11 @@
 package pl.sdacademy.zdjavapol33.java.zaawansowana.stream;
 
+import pl.sdacademy.zdjavapol33.java.zaawansowana.kolekcje.mapy.Osoba;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
 
@@ -35,6 +40,13 @@ public class StreamMain {
          * b) pofiltruj i wypisz zaczynajace się na A oraz majace więcej niż 5 liter
          *
          */
+        System.out.println("==============================");
+        System.out.println("=== Zadanie 1 ===");
+        System.out.println("==============================");
+
+        List<String> listaImion = Arrays.asList("Janek","Adam","Antonina","Ezechiel","Bonifacy");
+        listaImion.stream().filter(i -> i.startsWith("A")).forEach(System.out::println);
+
 
         /**
          * Zadanie 2.
@@ -43,16 +55,51 @@ public class StreamMain {
          * b) pofiltruj i wypisz osoby pełnoletnie z predykatem
          * c) wypisz listę "imie nazwisko"
          */
+        System.out.println("==============================");
+        System.out.println("=== Zadanie 2 ===");
+        System.out.println("==============================");
+        Osoba osoba1 = new Osoba("Jan","Kowalski",51);
+        Osoba osoba2 = new Osoba("Anna","Nowak",31);
+        Osoba osoba3 = new Osoba("Piotr","Anders",12);
+        Osoba osoba4 = new Osoba("Julia","Roszenko",10);
+        Osoba osoba5 = new Osoba("Joanna","Dark",66);
+        List<Osoba> listaOsob = Arrays.asList(osoba1,osoba2,osoba3,osoba4,osoba5);
+
+        //a)
+        listaOsob.stream().filter(o -> o.getWiek()>18).forEach(System.out::println);
+        //b)
+        listaOsob.stream().filter(maSkonczone18lat()).forEach(System.out::println);
+        //c)
+        listaOsob.stream().forEach(o-> System.out.println(o.getImie()+" "+o.getNazwisko()));
+
+
+
 
         /**
          * Zadanie 3.
          * Korzystajac z klasy Osoba i listy osób posortuj listę według wieku i wypisz.
          * Następnie posortuj po nazwisku i wypisz.
          */
+        System.out.println("==============================");
+        System.out.println("=== Zadanie 3 ===");
+        System.out.println("==============================");
 
+        //a)
+        listaOsob.stream().sorted((o1,o2)->Integer.compare(o1.getWiek(),o2.getWiek())).forEach(System.out::println);
+        System.out.println("\n");
+        //b)
+        // .sorted(Comparator.reverseOrder()) sortowanie odwrotnie po nazwisku
+        // w zdefiniowanej metodzie Osoba.compareTo()
+        listaOsob.stream().sorted((o1,o2)->o1.getNazwisko().compareTo(o2.getNazwisko())).forEach(System.out::println);
 
+    }
 
+    private static Comparator<Osoba> sortujWedlugWieku() {
+        return (o1, o2) -> Integer.compare(o1.getWiek(), o2.getWiek());
+    }
 
+    private static Predicate<Osoba> maSkonczone18lat() {
+        return o -> o.getWiek() > 18;
     }
 
     private static Predicate<String> equalStartM() {
